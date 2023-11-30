ba71532d3f69243569479602a2e1146b4c620af1
ErikFrankling <erik.frankling@frankling.se>
2023-11-30 15:03:29 +0100

    layers work and some mnist

--- src/layers.rs
+++ src/layers.rs
@@ -1,3 +1,5 @@
+use std::usize;
+
 use nalgebra::{SMatrix, SVector, DVector};
 use rand::distributions::Uniform;
 
@@ -7,46 +9,46 @@ pub trait Layer {
 
 pub struct Dense<const R: usize, const C: usize> {
     weights: SMatrix<f32, R, C>,
-    biases: SVector<f32, C>
+    biases: SVector<f32, R>
 }
 
 impl <const R: usize, const C: usize> Dense<R, C> {
-    pub fn new(&self) -> Self {
+    pub fn new() -> Self {
         let between = Uniform::from(0.0f32..1.0f32);
         let mut rng = rand::thread_rng();
 
         Dense {
             weights: SMatrix::<f32, R, C>::from_distribution(&between, &mut rng),
-            biases: SVector::<f32, C>::from_distribution(&between, &mut rng),
+            biases: SVector::<f32, R>::from_distribution(&between, &mut rng),
         }
     }
 }
 
 impl<const R: usize, const C: usize> Layer for Dense<R, C> {
     fn forward(&self, input: DVector<f32>) -> DVector<f32> {
-        todo!()//self.weights * input + self.biases
+        (self.weights * input + self.biases).data.as_slice().to_vec().into()
     }
 }
 
 pub struct Convolutional<const R: usize, const C: usize> {
     weights: SMatrix<f32, R, C>,
-    biases: SVector<f32, C>
+    biases: SVector<f32, R>
 }
 
 impl <const R: usize, const C: usize> Convolutional<R, C> {
-    pub fn new(&self) -> Self {
+    pub fn new() -> Self {
         let between = Uniform::from(0.0f32..1.0f32);
         let mut rng = rand::thread_rng();
 
         Convolutional {
             weights: SMatrix::<f32, R, C>::from_distribution(&between, &mut rng),
-            biases: SVector::<f32, C>::from_distribution(&between, &mut rng),
+            biases: SVector::<f32, R>::from_distribution(&between, &mut rng),
         }
     }
 }
 
 impl<const R: usize, const C: usize> Layer for Convolutional<R, C> {
     fn forward(&self, input: DVector<f32>) -> DVector<f32> {
-        todo!()//self.weights * input + self.biases
+        (self.weights * input + self.biases).data.as_slice().to_vec().into()
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,48 @@
 mod layers;
 
 use layers::*;
+use mnist::*;
+use nalgebra::{self, DMatrix};
 
 const HIDDEN_LAYERS: u8 = 2;
 const LAYER_SIZE: [usize; HIDDEN_LAYERS as usize] = [128, 64];
-const INPUTS: usize = 128 * 128;
+const INPUTS: usize = 28 * 28;
 const OUTPUTS: usize = 10;
 
 const LEARNING_RATE: f32 = 0.01;
 const EPOCHS: u32 = 1000;
 
 fn main() {
+    // Deconstruct the returned Mnist struct.
+   let Mnist {
+        trn_img,
+        trn_lbl,
+        tst_img,
+        tst_lbl,
+        ..
+    } = MnistBuilder::new()
+        .label_format_digit()
+        .training_set_length(50_000)
+        .test_set_length(10_000)
+        .finalize();
+
+    let train_data = DMatrix(trn_img.chunks(28 * 28));
+        .expect("Error converting images to Array3 struct")
+        .map(|x| *x as f32 / 256.0);
+
+    // Convert the returned Mnist struct to Array2 format
+    let train_labels: Array2<f32> = Array2::from_shape_vec((50_000, 1), trn_lbl)
+        .expect("Error converting training labels to Array2 struct")
+        .map(|x| *x as f32);
+
+    let _test_data = Array3::from_shape_vec((10_000, 28, 28), tst_img)
+        .expect("Error converting images to Array3 struct")
+        .map(|x| *x as f32 / 256.);
+
+    let _test_labels: Array2<f32> = Array2::from_shape_vec((10_000, 1), tst_lbl)
+        .expect("Error converting testing labels to Array2 struct")
+        .map(|x| *x as f32);
+
     let mut network: Vec<Box<dyn Layer>> = Vec::new();
     network.push(Box::new(Dense::<INPUTS, { LAYER_SIZE[0] }>::new()));
     network.push(Box::new(Convolutional::<{ LAYER_SIZE[0] }, { LAYER_SIZE[1] }>::new()));
